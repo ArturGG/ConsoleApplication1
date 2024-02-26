497d55a88f57c27176a11e3f9992d5a297a6b935
Nadenov Artur <arturad20@gmail.com>
2024-02-26 17:33:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+
+template<int n> struct Factorial {
+    static const int value = n * Factorial<n - 1>::value;
+};
+
+template<> struct Factorial<0> {
+    static const int value = 1;
+};
+
+int main()
+{
+    std::cout << "Factorial of 5: " << Factorial<5>::value << std::endl;
+    std::cout << "Factorial of 6: " << Factorial<6>::value << std::endl;
+    std::cout << "Factorial of 7: " << Factorial<7>::value << std::endl;
+    std::cout << "Factorial of 8: " << Factorial<8>::value << std::endl;
+    return 0;
+}
